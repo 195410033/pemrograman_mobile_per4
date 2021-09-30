95c825f01fa6186e829890f76312b55e0bc543d9
fauzi195410033 <fauzinaktkj@gmail.com>
2021-10-01 03:50:28 +0700

    pertemuan ke-4

---
+++ src/main/java/nim195410033/happybirthday/MainActivity.kt
@@ -0,0 +1,11 @@
+package nim195410033.happybirthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
